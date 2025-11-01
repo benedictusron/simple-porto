85e619ab59d6e03459f650c95ca3d889c4b5b42c
benedictusron <benedictusron99@gmail.com>
2025-11-01 20:51:50 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Navbar from './component/navbar.jsx'
+import Navbar from './component/Navbar.jsx'
 import Footer from './component/Footer.jsx'
 import 'animate.css';
 import Preloader from './component/Preloader.jsx'
